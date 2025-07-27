22eb48d02bff867490d790ce197688168542a88f
matt-auckland <mathewp.94@gmail.com>
2025-07-27 16:20:49 +1200

    Register api routes

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,7 +1,12 @@
 import { type FastifyInstance } from 'fastify';
+import { teamApiRoutes } from './team.ts';
+import { userApiRoutes } from './user.ts';
+import { authApiRoutes } from './auth.ts';
+import { standupApiRoutes } from './standup.ts';
 
 export function apiRoutes(fastify: FastifyInstance) {
-  fastify.get('/', async (request, reply) => {
-    return 'Hello World';
-  });
+  fastify.register(teamApiRoutes);
+  fastify.register(userApiRoutes);
+  fastify.register(standupApiRoutes);
+  fastify.register(authApiRoutes);
 }
