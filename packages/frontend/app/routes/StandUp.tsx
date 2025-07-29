5ed37091f45a4e295662d556e2fdfd6b1b403f49
matt-auckland <mathewp.94@gmail.com>
2025-07-29 21:03:40 +1200

    Fix type errors

--- packages/frontend/app/routes/StandUp.tsx
+++ packages/frontend/app/routes/StandUp.tsx
@@ -1,18 +1,17 @@
-import type { Route } from './+types/StandUp';
-import { Welcome } from '../welcome/welcome';
+// this file isn't used in the routes at the moment so the types aren't generated
+// import type { Route } from './+types/';
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: 'Stand Up DATE - TEAAMNAME' },
-    { name: 'description', content: 'DATE Stand up for TEAMNAME' },
-  ];
-}
+// export function meta({}: Route.MetaArgs) {
+//   return [
+//     { title: 'Stand Up DATE - TEAAMNAME' },
+//     { name: 'description', content: 'DATE Stand up for TEAMNAME' },
+//   ];
+// }
 
 export default function StandUpRoute() {
   return (
     <>
       <h1>Stand up</h1>
-      <Welcome />
     </>
   );
 }
